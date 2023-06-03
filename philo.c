85beb31e203bed88cb4198df45c6c1734086ca43
qnert <127817637+qnert@users.noreply.github.com>
2023-06-03 16:54:07 +0200

    headers included and some thread experiments

--- philo.c
+++ philo.c
@@ -6,13 +6,46 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 15:39:33 by skunert           #+#    #+#             */
-/*   Updated: 2023/06/03 15:41:04 by skunert          ###   ########.fr       */
+/*   Updated: 2023/06/03 16:53:26 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+pthread_mutex_t LOCK;
+
+void	*routine(void *arg)
+{
+	int	nb;
+
+	nb = *(int *) arg;
+	if (nb > -1)
+	ft_printf("%d\n", getpid());
+	return (NULL);
+}
 int	main(void)
 {
-	ft_printf("Hallo Welt\n");
+	pthread_t	philo[3];
+	int			i;
+	pid_t		pid;
+
+	pid = fork();
+	i = 0;
+	if (pid == 0)
+	{
+		printf("LOL\n");
+	}
+	while (i < 2)
+	{
+		pthread_create(&philo[i], NULL, routine, (void *)&i);
+		i++;
+	}
+	i = 0;
+	while (i < 2)
+	{
+		pthread_join(philo[i], NULL);
+		pthread_detach(philo[i]);
+		i++;
+	}
+	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 15:40:01 by skunert           #+#    #+#             */
-/*   Updated: 2023/06/03 15:40:37 by skunert          ###   ########.fr       */
+/*   Updated: 2023/06/03 16:39:38 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 #define PHILO_H
 
 # include "./ft_printf/ft_printf.h"
+# include <stdio.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <fcntl.h>
 
 #endif
