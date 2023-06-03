9f2df709c4fd343bf249221cd05192229d07531b
qnert <127817637+qnert@users.noreply.github.com>
2023-06-03 17:05:38 +0200

    lol

--- philo.c
+++ philo.c
@@ -6,23 +6,21 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 15:39:33 by skunert           #+#    #+#             */
-/*   Updated: 2023/06/03 16:53:26 by skunert          ###   ########.fr       */
+/*   Updated: 2023/06/03 17:04:39 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-pthread_mutex_t LOCK;
-
 void	*routine(void *arg)
 {
-	int	nb;
+	int	*nb;
 
-	nb = *(int *) arg;
-	if (nb > -1)
-	ft_printf("%d\n", getpid());
+	nb = (int *)arg;
+	ft_printf("Philosopher %d from process: %d\n", *nb, getpid());
 	return (NULL);
 }
+
 int	main(void)
 {
 	pthread_t	philo[3];
@@ -31,20 +29,11 @@ int	main(void)
 
 	pid = fork();
 	i = 0;
-	if (pid == 0)
-	{
-		printf("LOL\n");
-	}
+	printf("%d\n", pid);
 	while (i < 2)
 	{
 		pthread_create(&philo[i], NULL, routine, (void *)&i);
-		i++;
-	}
-	i = 0;
-	while (i < 2)
-	{
 		pthread_join(philo[i], NULL);
-		pthread_detach(philo[i]);
 		i++;
 	}
 	return (0);
