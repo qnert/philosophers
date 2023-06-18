b7068aa20172eab56fe9bc2d28b9cffc6be83c60
Simon Kunert <skunert@1-E-15.42heilbronn.de>
2023-06-18 15:31:21 +0200

    input check done

--- check.c
+++ check.c
@@ -6,21 +6,11 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 16:12:12 by skunert           #+#    #+#             */
-/*   Updated: 2023/06/17 17:12:13 by skunert          ###   ########.fr       */
+/*   Updated: 2023/06/18 15:30:28 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-int	ft_strlen(char *str)
-{
-	int	len;
-
-	len = 0;
-	while (str[len] != '\0')
-		len++;
-	return (len);
-}
 
 int	ft_atoi(const char *str)
 {
@@ -51,18 +41,17 @@ int	ft_atoi(const char *str)
 int	check_nbs(char *str)
 {
 	int	i;
+	int	check;
 
 	i = 0;
+	check = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] < '0' || str[i] > '9')
-		{
-			if (i == 0 && str[i] == 43)
-			{
-			}
-			else
+		if (str[i] > '0' && str[i] <= '9')
+			check = 1;
+		if (str[i] < '1' || str[i] > '9')
+			if ((str[i] == '+' && check == 1) || str[i] != '+')
 				return (0);
-		}
 		i++;
 	}
 	return (1);
@@ -72,9 +61,7 @@ int	check_input(char **argv)
 {
 	int	i;
 
-	i = 2;
-	if (check_nbs(argv[1]) == 0)
-		return (0);
+	i = 1;
 	while (argv[i] != NULL)
 	{
 		if (check_nbs(argv[i]) == 0)
--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 15:39:33 by skunert           #+#    #+#             */
-/*   Updated: 2023/06/17 17:01:23 by skunert          ###   ########.fr       */
+/*   Updated: 2023/06/18 15:14:01 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 int	main(int argc, char **argv)
 {
 	if (argc == 5)
+	{
+		if (check_input(argv) == 0)
+			write(1, "Input Error\n", 12);
+	}
+	if (argc == 6)
 	{
 		if (check_input(argv) == 0)
 			write(1, "Input Error\n", 12);
