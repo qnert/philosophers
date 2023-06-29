12811daa77250d220b2d3ff1d62c15c1936b049b
Simon Kunert <skunert@2-B-7.42heilbronn.de>
2023-06-29 10:59:02 +0200

    makefile fix

--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -6,14 +6,13 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 10:46:48 by skunert           #+#    #+#             */
-/*   Updated: 2023/06/29 10:47:53 by skunert          ###   ########.fr       */
+/*   Updated: 2023/06/29 10:58:42 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_BONUS_H
 # define PHILO_BONUS_H
 
-# include "./ft_printf/ft_printf.h"
 # include <stdio.h>
 # include <pthread.h>
 # include <unistd.h>
