d7fd60db0d8b540f48976f600cba71f505b90ae7
Simon Kunert <skunert@1-A-6.42heilbronn.de>
2023-07-10 16:04:09 +0200

    deadlock avoiding helgrind fixed

--- philo/srcs/routine_funcs.c
+++ philo/srcs/routine_funcs.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 14:51:39 by skunert           #+#    #+#             */
-/*   Updated: 2023/07/10 13:35:00 by skunert          ###   ########.fr       */
+/*   Updated: 2023/07/10 15:33:39 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,20 @@ void	printf_safe(char *msg, t_philo *philo, long time)
 
 int	first_routine(t_philo *philo)
 {
-	pthread_mutex_lock(&philo->dinnertable->forks[philo->id - 1]);
+	if (philo->id == philo->dinnertable->nb_of_philos)
+		pthread_mutex_lock(&philo->dinnertable->forks[philo->id
+			% philo->dinnertable->nb_of_philos]);
+	else
+		pthread_mutex_lock(&philo->dinnertable->forks[philo->id - 1]);
 	printf_safe("has taken a fork", philo, get_time(philo->dinnertable->birth));
 	if (philo->dinnertable->nb_of_philos == 1)
 		return (pthread_mutex_unlock(&philo->dinnertable->forks[philo->id - 1]),
 			-1);
-	pthread_mutex_lock(&philo->dinnertable->forks[philo->id
-		% philo->dinnertable->nb_of_philos]);
+	if (philo->id == philo->dinnertable->nb_of_philos)
+		pthread_mutex_lock(&philo->dinnertable->forks[philo->id - 1]);
+	else
+		pthread_mutex_lock(&philo->dinnertable->forks[philo->id
+			% philo->dinnertable->nb_of_philos]);
 	printf_safe("has taken a fork", philo, get_time(philo->dinnertable->birth));
 	printf_safe("is eating", philo, get_time(philo->dinnertable->birth));
 	philo->times_eaten += 1;
